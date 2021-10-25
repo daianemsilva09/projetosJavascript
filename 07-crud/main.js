3296c59d0b0c7b4e4e98035935b371b4f6b1d692
Daiane Silva <daianemsilva09@gmail.com>
2021-10-25 15:55:01 -0300

    Crud - READ

--- 07-crud/main.js
+++ 07-crud/main.js
@@ -6,6 +6,27 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+    nome:"Daian",
+    email:"daiane@gamil.com",
+    celular:"994012140",
+    cidade:"Cps"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setlocalStorage = (dbClient) => localStorage.setItem ("db_client", JSON.stringify(dbClient))
+//Crud - create  read apdate e delite
+
+//Crud - READ
+const createClient = (client) =>{
+    const dbClient = getLocalStorage()
+    dbClient.push(client)
+    setlocalStorage(dbClient)
+    
+}
+ 
+
+//eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
